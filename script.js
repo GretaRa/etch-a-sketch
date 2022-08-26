7a83781733b86eecdc102d52f2e85230f92858a8
Greta Ra <ggretute88@gmail.com>
2022-08-26 17:10:52 +0200

    Ask for user input for sizing

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-//Create a webpage with a 16x16 grid of square divs
+//Create reset button in HTML
+const resetBtn = document.createElement('button');
+resetBtn.innerHTML = 'Reset';
+document.body.insertBefore(resetBtn, document.body.children[0]).className = 'resetBtn';
+
+resetBtn.addEventListener('click', () => {
+    let rows = Number(prompt('Please choose canvas width between 1 and 100 squares', 16));
+    let cols = Number(prompt('Please choose canvas height between 1 and 100 squares', 16));
+
+    if (cols > 100 || cols < 1 || rows > 100 || rows < 1){
+        alert('Choose a number between 1 and 100');
+        return;
+    } else {
+        createCanvas(rows, cols);
+    }
+})
 
 const container = document.querySelector('.container');
 
+//Create a webpage with a 16x16 grid of square divs
 function createCanvas(rows,cols){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for(c = 0; c < (rows * cols); c++){
         let box = document.createElement('div');
-        box.innerText = (c + 1);
+        box.innerText = ('');
         container.appendChild(box).className = 'box';
 
         //Change div color when hovered
@@ -16,8 +32,8 @@ function createCanvas(rows,cols){
         })
     }
 }
-
 createCanvas(16,16);
+
 
 
 
